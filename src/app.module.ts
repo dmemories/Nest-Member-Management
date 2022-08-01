d60a09e152a2626ad05d7d53c7c5d1a6c14f5490
Alkaiser <spyhack13@yahoo.com>
2022-08-01 22:00:32 +0700

    Initial Project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppService } from './app.service';
+import { MemberModule } from './member/member.module';
+import { AuthModule } from './auth/auth.module';
+
+@Module({
+  imports: [MemberModule, AuthModule],
+  controllers: [],
+  providers: [AppService],
+})
+export class AppModule {}
